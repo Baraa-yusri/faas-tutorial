398f614545931c6bdab43771dead8f8470bb7424
serafinplattner <73935011+serafinplattner@users.noreply.github.com>
2022-09-28 09:59:20 +0200

    add policy for accessing S3 to iam role

--- Task3/Final/main.tf
+++ Task3/Final/main.tf
@@ -22,15 +22,15 @@ data "archive_file" "lambda_terraform_test" {
 # Test Function "plus"
 resource "aws_lambda_function" "aws_function" {
   function_name    = "plus"
-  filename         = data.archive_file.lambda_hello_world.output_path
+  filename         = data.archive_file.lambda_terraform_test.output_path
   runtime          = "python3.9"
   handler          = "function.lambda_handler"
-  source_code_hash = filebase64sha256(data.archive_file.lambda_hello_world.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.lambda_terraform_test.output_path)
   role             = aws_iam_role.lambda_exec.arn
   #layers = ["arn:aws:lambda:us-east-1:770693421928:layer:Klayers-p39-pillow:1"] #for face recognition
 }
 
-resource "aws_iam_role" "lambda_exec" {
+resource "aws_iam_role" "lambda_exec2" {
   name = "github_actions_deploy"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -49,4 +49,9 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy2" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
